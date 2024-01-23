ed895e96e593af7eb5a4297ce69a991a8d12ea13
Matthew Brady <w3matt@gmail.com>
2024-01-23 16:21:27 +0000

    regex further fix

--- bd_scan_yocto/process.py
+++ bd_scan_yocto/process.py
@@ -126,8 +126,8 @@ def proc_pkg_files():
                 global_values.recipe_layer_dict[recipe] in global_values.exclude_layers:
             continue
 
-        download_regex = re.compile(f"^{recipe}[_-]v.*{ver}[.-].*$")
-        pkg_regex = re.compile(f"^(lib).*{recipe}\d*[_-]v.*{ver}[+.-].*\.{global_values.image_pkgtype}")
+        download_regex = re.compile(f"^{recipe}[_-]v?{ver}[.-].*$")
+        pkg_regex = re.compile(f"^(lib)?{recipe}\d*[_-]v?{ver}[+.-].*\.{global_values.image_pkgtype}")
 
         for path, file in zip(download_paths_list, download_files_list):
             # Check for recipe and version
